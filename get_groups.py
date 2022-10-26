4b60f224db309977e2fb0302aca888376d4cf530
zappuf <frank@codeocean.com>
2022-10-25 23:19:59 -0400

    removed try/except

--- get_groups.py
+++ get_groups.py
@@ -5,16 +5,13 @@ import sys
 def get_groups(filename="sample_sheet.csv"):
     filepath = subprocess.check_output(["find", "../data", "-name", filename]).decode("utf-8").strip()
     groups_set = set()
-    try:
-        with open(f"{filepath}") as infile:
-            lines = infile.readlines()
-            for line in lines:
-                line = line.strip()
-                line_group = line.split(",")[0]
-                groups_set.add(line_group)
-            return ",".join(list(groups_set))
-    except ValueError as ve:
-        print(f"The following error occurred: {ve}")
+    with open(f"{filepath}") as infile:
+        lines = infile.readlines()
+        for line in lines:
+            line = line.strip()
+            line_group = line.split(",")[0]
+            groups_set.add(line_group)
+        return ",".join(list(groups_set))
 
 if len(sys.argv) > 1:
     print(get_groups(sys.argv[1]))
